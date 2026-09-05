01af2a9bbff7cf15261b085ec26d7861efd29152
Anton Shabouta <zloyusr@gmail.com>
2026-09-05 15:01:26 +0300

    [atlas+21]: Replace ambiguous isset checks

--- database/migrations/create_pinax_tables.php
+++ database/migrations/create_pinax_tables.php
@@ -40,7 +40,7 @@ return new class extends Migration {
     {
         $tenancy = (array) config('phpinnacle-pinax.tenancy');
 
-        if (isset($tenancy['model']) && class_exists($tenancy['model'])) {
+        if (($tenancy['model'] ?? null) !== null && class_exists($tenancy['model'])) {
             $table
                 ->foreignIdFor($tenancy['model'], 'tenant_id')
                 ->after('id')
