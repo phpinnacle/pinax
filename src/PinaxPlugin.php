52db680c58534dfa040d8b57941fdab5dc1b0f38
Anton Shabouta <zloyusr@gmail.com>
2026-09-05 15:16:16 +0300

    [atlas+20]: Simplify inline returns

--- src/PinaxPlugin.php
+++ src/PinaxPlugin.php
@@ -9,6 +9,7 @@ class PinaxPlugin implements Plugin
 {
     public static function get(): static
     {
+        // @mago-expect lint:inline-variable-return
         /** @var static $plugin */
         $plugin = filament(app(static::class)->getId());
 
